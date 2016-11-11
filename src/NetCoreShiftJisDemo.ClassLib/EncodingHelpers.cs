fa9404f27eed8e99d27bba9b76a46f18a278c678
Ovchinnikov <ovchinnikov@smartdriving.io>
2016-11-11 17:46:31 +0300

    Add project files.

---
+++ src/NetCoreShiftJisDemo.ClassLib/EncodingHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreShiftJisDemo.ClassLib
+{
+    public static class EncodingHelpers
+    {
+        static EncodingHelpers()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        public static Encoding GetShiftJis()
+        {
+            return Encoding.GetEncoding("Shift-JIS");
+        }
+    }
+}
---
+++ src/NetCoreShiftJisDemo.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetCoreShiftJisDemo.ClassLib;
+
+namespace NetCoreShiftJisDemo.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var encoding = EncodingHelpers.GetShiftJis();
+        }
+    }
+}
